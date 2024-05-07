80d8d24e8e882bab383cdb6398add20b5ab677d8
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2024-05-08 02:35:49 +0600

    user code has been added

--- src/User.java
+++ src/User.java
@@ -1,2 +1,28 @@
-public class User {
+import java.util.ArrayList;
+import java.util.List;
+
+class User {
+    private String email;
+    private List<Movie> favorites;
+
+    public User(String email) {
+        this.email = email;
+        this.favorites = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Movie> getFavorites() {
+        return favorites;
+    }
+
+    public void addFavorite(Movie movie) {
+        favorites.add(movie);
+    }
+
+    public void removeFavorite(Movie movie) {
+        favorites.remove(movie);
+    }
 }
