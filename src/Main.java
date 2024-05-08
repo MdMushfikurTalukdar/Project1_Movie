c472c3306ca9ddae9ada24f37cad72e81d9caf8f
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2024-05-08 15:25:21 +0600

    Main class has been updated using

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MovieApp movieApp = new MovieApp();
+
+        // Sample movies
+        movieApp.addMovie(new Movie("The Shawshank Redemption", List.of("Tim Robbins", "Morgan Freeman"), "Drama", "1994", 25000000));
+        movieApp.addMovie(new Movie("The Godfather", List.of("Marlon Brando", "Al Pacino"), "Crime", "1972", 6000000));
+        movieApp.addMovie(new Movie("The Dark Knight", List.of("Christian Bale", "Heath Ledger"), "Action", "2008", 185000000));
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nWelcome to Movie App");
+            System.out.println("1. Register");
+            System.out.println("2. Search Movies");
+            System.out.println("3. Add to Favorites");
+            System.out.println("4. Remove from Favorites");
+            System.out.println("5. View Favorites");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if(choice==1){
+                movieApp.registerUser();
+            }
+            else if (choice==2){
+                System.out.print("Enter search query: ");
+                String query = scanner.nextLine();
+                movieApp.searchMovies(query);
+            }
+            else if (choice==3){
+                // Add to favorites
+            }
+            else if (choice==4){
+                // Remove from favorites
+            }
+            else if (choice==5){
+                // View favorites
+            }
+            else if (choice==6){
+                System.out.println("Thank you for using Movie App. Goodbye!");
+                System.exit(0);
+                break;
+            }
+            else{
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
